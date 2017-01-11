6aae6f2c70c7e2b5387dd1ffca54fc3c358ca43b
zhanghangfeng <zjsegzhf@hotmail.com>
2017-01-11 16:01:53 +0800

    复合数据类型-JSON

--- src/main.go
+++ src/main.go
@@ -1,34 +1,19 @@
 package main
 
 import (
-	"encoding/json"
+	"func/github"
+	"os"
 	"log"
 	"fmt"
 )
 
-type Movie struct {
-	Title string
-	Year int `json:"released"`
-	Color bool `json:"color,omitempty"`
-	Actors []string
-}
-
-var movies = []Movie{
-	{Title: "Casablanca", Year: 1942, Color: false, Actors: []string{"Humphery Bogart", "Ingrid Bergaman"}},
-	{Title: "Cool Hand Luke", Year: 1967, Color: true, Actors: []string{"Paul Newman"}},
-	{Title: "Bullitt", Year: 1968, Color: true, Actors: []string{"Steve McQueen", "Jacqueline Bisset"}},
-}
-
 func main() {
-	data, err := json.MarshalIndent(movies, "", "    ")
+	result, err := github.SearchIssues(os.Args[1:])
 	if err != nil {
-		log.Fatalf("Json marshling failed: %s", err)
+		log.Fatal(err)
 	}
-	fmt.Printf("%s\n", data)
-
-	var titles []struct{Title string}
-	if err := json.Unmarshal(data, &titles); err != nil {
-		log.Fatalf("Json unmarshaling failed, %s", err)
+	fmt.Printf("%d issues:\n", result.TotalCount)
+	for _, item := range result.Items {
+		fmt.Printf("#%-5d %s %s %v\n", item.Number, item.User.Login, item.Title, item.CreatedAt)
 	}
-	fmt.Println(titles)
 }
