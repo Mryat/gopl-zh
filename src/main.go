84e31d63628e0a1e894cc1da5131ef6557ba1a59
zhanghangfeng <zjsegzhf@hotmail.com>
2017-01-11 14:32:10 +0800

    记录学习gopl-zh的代码

---
+++ src/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"fmt"
+)
+
+type Movie struct {
+	Title string
+	Year int `json:"released"`
+	Color bool `json:"color,omitempty"`
+	Actors []string
+}
+
+var movies = []Movie{
+	{Title: "Casablanca", Year: 1942, Color: false, Actors: []string{"Humphery Bogart", "Ingrid Bergaman"}},
+	{Title: "Cool Hand Luke", Year: 1967, Color: true, Actors: []string{"Paul Newman"}},
+	{Title: "Bullitt", Year: 1968, Color: true, Actors: []string{"Steve McQueen", "Jacqueline Bisset"}},
+}
+
+func main() {
+	data, err := json.MarshalIndent(movies, "", "    ")
+	if err != nil {
+		log.Fatalf("Json marshling failed: %s", err)
+	}
+	fmt.Printf("%s\n", data)
+
+	var titles []struct{Title string}
+	if err := json.Unmarshal(data, &titles); err != nil {
+		log.Fatalf("Json unmarshaling failed, %s", err)
+	}
+	fmt.Println(titles)
+}
